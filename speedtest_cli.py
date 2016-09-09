36d7e8659e38a86899cd8e074de10009e1846523
OurFlagIsMined <OurFlagIsMined@users.noreply.github.com>
2016-09-08 22:09:34 -0400

    Testing

--- speedtest_cli.py
+++ speedtest_cli.py
@@ -431,7 +431,8 @@ def closestServers(client, all=False):
     urls = [
         # 'https://www.speedtest.net/speedtest-servers-static.php',
         # 'http://c.speedtest.net/speedtest-servers-static.php', # fails
-        '://www.speedtest.net/speedtest-servers.php',
+        # '://www.speedtest.net/speedtest-servers.php', # fails
+        'https://www.speedtest.net/speedtest-servers.php',
         'http://c.speedtest.net/speedtest-servers.php',
     ]
     errors = []
